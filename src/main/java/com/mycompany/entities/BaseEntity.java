6b0fbdfa24cf99f77ed4de3cafd2b09a81c91d6f
hungvcap02 <hungvcap02@gmail.com>
2024-08-02 10:04:28 +0700

    Add Entities

---
+++ src/main/java/com/mycompany/entities/BaseEntity.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.entities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Legion
+ */
+public class BaseEntity {
+    private static final String DATABASE = "library_hust_managerment"; 
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    
+    static Connection conn = null;
+    static PreparedStatement statement = null;
+
+    static void open() {
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + DATABASE, USERNAME, PASSWORD); // jdbc:mysql://localhost:3306/CS3360/root/
+        } catch (SQLException ex) {
+            Logger.getLogger(BaseEntity.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    static void close() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(BaseEntity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(BaseEntity.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        conn = null;
+        statement = null;
+    }
+}
---
+++ src/main/java/com/mycompany/entities/CatalogEntity.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.entities;
+
+import static com.mycompany.entities.BaseEntity.close;
+import static com.mycompany.entities.BaseEntity.conn;
+import static com.mycompany.entities.BaseEntity.open;
+/**
+ *
+ * @author Legion
+ */
+public class CatalogEntity {
+    
+}
