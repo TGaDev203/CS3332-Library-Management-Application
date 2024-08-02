3faf17b863d177ca293df1224c6d78aaee5b40a5
hungvcap02 <hungvcap02@gmail.com>
2024-08-02 10:36:03 +0700

    import package of BaseEntity

--- src/main/java/com/mycompany/entities/BorrowBookEntity.java
+++ src/main/java/com/mycompany/entities/BorrowBookEntity.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.entities;
-
+import static com.mycompany.entities.BaseEntity.close;
+import static com.mycompany.entities.BaseEntity.conn;
+import static com.mycompany.entities.BaseEntity.open;
 /**
  *
  * @author Legion
